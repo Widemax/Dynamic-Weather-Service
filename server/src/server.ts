81da7358d4c0ef65a0d2dd16c1f46f659c075365
Widemax <116132865+Widemax@users.noreply.github.com>
2025-03-26 15:01:26 -0700

    Slight Fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve static files from the client/dist folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
